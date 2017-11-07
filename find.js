1c2538de8ae78cc670fcf6f1529192546f391100
zaturnvalley <zacharycmason@gmail.com>
2017-11-07 14:41:36 -0800

    added find helper method

---
+++ find.js
@@ -0,0 +1,53 @@
+// Find helper method
+
+// for loop old example
+var users = [
+{ name: 'Jill'},
+{ name: 'Alex'},
+{ name: 'Bill'}
+];
+
+var user;
+
+for (var i = 0; i < users.length; i++){
+  if (users[i].name === 'Alex'){
+    user = users[i];
+    break; // exit if conditional met
+  }
+}
+
+user;
+
+// Find Helper Method Example
+
+users.find(function(user){
+ return user.name === 'Alex';
+});
+
+function Car(model){
+  this.model = model;
+}
+var cars = [
+  new Car('Buick'),
+  new Car('Camaro'),
+  new Car('Focus')
+];
+
+cars.find(function(car){
+  return car.model === 'Focus';
+});
+
+var posts = [
+{ id: 1, title: "New Post" },
+{ id: 2, title: "Old Post" }
+];
+
+var comment = { postId: 1, content: 'Great Post' };
+
+function postForComment(posts, comment){
+return posts.find(function(post){
+  return post.id === comment.postId;
+});
+
+postForComment(posts, comment);
+}
