892200b0ca40038f5cd7a9413791ad68d2135983
zaturnvalley <zacharycmason@gmail.com>
2017-11-07 14:42:52 -0800

    fixed syntax error

--- find.js
+++ find.js
@@ -45,9 +45,9 @@ var posts = [
 var comment = { postId: 1, content: 'Great Post' };
 
 function postForComment(posts, comment){
-return posts.find(function(post){
-  return post.id === comment.postId;
-});
-
+  return posts.find(function(post){
+    return post.id === comment.postId;
+  });
+}
 postForComment(posts, comment);
 }
