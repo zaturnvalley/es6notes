3ab296646a641c3524064600124e5c4da75a9d10
zaturnvalley <zacharycmason@gmail.com>
2017-11-16 13:09:05 -0800

    added reduce examples

--- reduce.js
+++ reduce.js
@@ -11,3 +11,38 @@ for (var i = 0; i < numbers.length; i++){
 numbers.reduce(function(sum, number){
   return sum + number;
 }, 0);
+
+// example
+var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+var deskTypes = desks.reduce(function(prev, desk) {
+    if (desk.type === 'sitting') {
+        ++prev.sitting; 
+    }
+    if (desk.type === 'standing') {
+        ++prev.standing; 
+    }
+    return prev;
+}, { sitting: 0, standing: 0 });
+
+// example using find
+var numbers = [1, 1, 2, 3, 4, 4];
+
+function unique(array) {
+    return array.reduce(function(prev, arrNum) {
+        if(!prev.find(function(prevNum){
+            return prevNum === arrNum;
+        })){
+            prev.push(arrNum);
+        } return prev;
+    }, []);
+  
+}
+
+unique(numbers);
