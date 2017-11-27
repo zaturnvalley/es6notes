4f7fc1fdc44e6b6ee430e93455346dda2092fcff
zaturnvalley <zacharycmason@gmail.com>
2017-11-27 14:08:47 -0800

    es5 example prototypical inheritance

--- classes.js
+++ classes.js
@@ -12,3 +12,23 @@ Car.prototype.drive = function(){
 
 const Car = new Car({ title: 'Focus'});
 car.drive(); // returns vroom
+
+// new obj inherits from Car
+
+function Toyota(options){
+  Car.call(this,options);
+  this.color = options.color;
+}
+
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+
+Toyota.prototype.honk = function(){
+  return 'beep';
+}
+const toyota = new Toyota({ color: 'red', title: 'Daily Driver'});
+toyota;
+toyota.drive();
+toyota.honk();
+
+// ES6 refactored version
