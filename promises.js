6e2ba8d28780f997065bd3fb7a6b09bfe64a0046
zaturnvalley <zacharycmason@gmail.com>
2017-12-05 19:19:12 -0800

    fetch req example

--- promises.js
+++ promises.js
@@ -1,7 +1,15 @@
 // promises
 
 promise = new Promise((resolve, reject) => {
+  // setTimeout(() => {
+  //   resolve();
+  // }, 3000);
+
+ var request = new XMLHttpRequest();
+ // make request
+ request.onload = () => {
   resolve();
+ } 
 });
 // promise = new Promise((resolve, reject) => {
 //   reject();
@@ -11,3 +19,9 @@ promise
   .then(() => console.log('finally finished'))
   .then(() => console.log('this too'))
   .catch(() => console.log('something went wrong'));
+
+  // Ajax Request with Fetch
+
+  url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then(response => response.json());
